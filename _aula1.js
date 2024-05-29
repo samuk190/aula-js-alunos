ca095abbe210d78867d9bedecbb933292837286d
Samuel Wallace <voojj3054@gmail.com>
2024-05-28 21:03:20 -0300

    Aula Inicial, modulo 0, inicio de projeto

---
+++ _aula1.js
@@ -0,0 +1,34 @@
+
+let inicial = "L" // Char
+let nome = "Lucas"; // String == Lista de Char
+let numero = 1 //Int small int, big int 0 até 16 mil, 0 até 64 mil
+let preço =  1.5241412  //Double ou Float
+
+
+let objeto = { }  //Object
+
+let pessoa1 = "Gustavo";
+let pessoa2 = "Fabio";
+let pessoa3 = "Ana";
+
+
+let carroHyundai = {
+    marca:"Hyundai",
+    porta: 4,
+    motor: {
+        cilindro: 4,
+        potencia: 1.0,
+        valvula: 16
+    }
+}
+
+let carroVW = {
+    marca: "VW",
+    porta: 4,
+    motor: {
+        cilindro: 4,
+        potencia: 1.0,
+        valvula: 16
+    }
+}
+let lista = [carroVW,carroHyundai] //List
---
+++ aula1.js
@@ -0,0 +1,62 @@
+
+
+let marca = 'Honda';
+let modelo = 'civic';
+let ano = 2004;
+let motor = 1.7;
+let cor = 'prata';
+let carro = { name:"Honda", marca: marca, modelo: modelo, ano: ano, motor: motor, cor: cor };
+let carroFusca = { name:"Fusca",marca: 'VW', modelo: 1980, ano: 1981, motor: 1.5, cor: 'vermelho' };
+// console.log(carro.marca);
+
+
+// //Criar um objeto do tipo Pessoa, com atributo nome, age, gender, height, weight
+// //Criar um objeto, do tipo Carro, com atributo marca modelo ano cor, motor
+// //Criar uma lista contendo a pessoa, e o carro.
+let carroDosSonhos = false;
+
+
+
+// && - AND
+// || OR
+// ! NOT
+// ==
+let tenhoDinheiro = true;
+        //tenhoDinheiro === false -> false
+        //!tenhoDinheiro === false -> verdade
+
+
+let listaCarros = [carro,carroFusca];
+
+
+listaCarros.forEach(carro => {
+    console.log(carro.name);
+});
+// console.log(listaCarros);
+
+
+// for (let index = 0; index < listaCarros.length; index++) {
+
+//     console.log("Estou acessando qual carro?",index);
+//     console.log("O Nome do carro é:",listaCarros[index].name);
+
+
+// }
+
+// let index = 0;
+// while (index != 2)  {
+//     console.log("TESTE");
+//     index++;
+// }
+
+
+
+// let list = [carro, pessoa];
+// console.log(list);
+
+// console.log(list.filter(x => x.age === 19));
+
+
+// console.log(lista)
+
+
